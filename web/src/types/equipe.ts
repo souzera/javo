b63be9f9b7808a1be2cf118e598036c681e3effc
Matheus Barbosa <ps.matheusb@gmail.com>
2024-01-13 11:57:20 -0300

    :label: adicionando e atualizando tipos

---
+++ web/src/types/equipe.ts
@@ -0,0 +1,11 @@
+export interface Equipe{
+    id: number;
+    nome: string;
+}
+
+export const sample: Equipe[]=[
+    {id: 1, nome: 'Suporte'},
+    {id: 2, nome: 'Desenvolvimento'},
+    {id: 3, nome: 'Infraestrutura'},
+    {id: 4, nome: 'Teste'},
+]
---
+++ web/src/types/usuario.ts
@@ -0,0 +1,14 @@
+interface Usuario {
+  id: number;
+  apelido: string;
+  id_equipe: number;
+}
+
+export const sample: Usuario[] = [
+  { id: 1, apelido: 'João', id_equipe: 1 },
+  { id: 2, apelido: 'Maria', id_equipe: 1 },
+  { id: 3, apelido: 'Matheus', id_equipe: 2 },
+  { id: 4, apelido: 'Junior', id_equipe: 2 },
+  { id: 5, apelido: 'Ana', id_equipe: 2 },
+  { id: 6, apelido: 'Maria', id_equipe: 2 },
+]
