f61dafd5f23ef513b9b4ffdeaf070ff9e094f89b
Matheus Barbosa <ps.matheusb@gmail.com>
2024-01-14 15:51:47 -0300

    :package: alterando icones para o react icons

--- web/src/components/Equipes/CriaEquipeButton.tsx
+++ web/src/components/Equipes/CriaEquipeButton.tsx
@@ -5,7 +5,7 @@ export function CriarEquipeButton() {
         type="button"
         className="flex items-center justify-center px-4 py-2 text-lg font-bold text-white rounded-full border-white border-2 transition ease-in-out hover:scale-105"
       >
-        
+        <i className="fa-solid fa-circle-plus text-white"></i>
         <span>Criar Equipe</span>
       </button>
     </>
