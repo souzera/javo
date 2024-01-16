27fe20dbacb71f53a79f31d57611a5369569ef53
Matheus Barbosa <ps.matheusb@gmail.com>
2024-01-15 21:35:21 -0300

    :recycle: adicionando modal ao botao criar equipe

--- web/src/components/Equipes/CriaEquipeButton.tsx
+++ web/src/components/Equipes/CriaEquipeButton.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { HiPlusCircle } from "react-icons/hi";
 
-import Modal from "react-modal"
+import {Modal} from "antd"
 
 export function CriarEquipeButton() {
 
@@ -22,14 +22,17 @@ export function CriarEquipeButton() {
   return (
     <>
       <button
+        onClick={showModal}
         type="button"
         className="flex items-center justify-center gap-2 px-4 py-2 text-lg font-bold text-white rounded-full border-white border-2 transition ease-in-out duration-1000 hover:scale-105 w-full"
       >
         <HiPlusCircle size={24} />
         <span>Criar Equipe</span>
       </button>
-      <Modal>
-
+      <Modal title="Basic Modal" open={modalIsOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
       </Modal>
     </>
 
