802e02a2ce1a9c2b03bb7c5bd9b03cc435c24a6b
Matheus Barbosa <ps.matheusb@gmail.com>
2024-01-15 21:32:31 -0300

    :package: instalando antd

--- web/src/components/Equipes/CriaEquipeButton.tsx
+++ web/src/components/Equipes/CriaEquipeButton.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import { HiPlusCircle } from "react-icons/hi";
 
+import Modal from "react-modal"
+
 export function CriarEquipeButton() {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const showModal = () => {
+    setModalIsOpen(true);
+  }
+
+  const handleOk = () => {
+    setModalIsOpen(false);
+  }
+
+  const handleCancel = () => {
+    setModalIsOpen(false);
+  }
+
   return (
     <>
       <button
         type="button"
         className="flex items-center justify-center gap-2 px-4 py-2 text-lg font-bold text-white rounded-full border-white border-2 transition ease-in-out duration-1000 hover:scale-105 w-full"
       >
-        <HiPlusCircle size={24}/>
+        <HiPlusCircle size={24} />
         <span>Criar Equipe</span>
       </button>
+      <Modal>
+
+      </Modal>
     </>
+
   )
 }
